6f464d0e27844ffe1bfb828ce693ba1dde842115
dev-rb <rbatra486@gmail.com>
2023-07-14 21:53:55 -0400

    Update dummy data

--- src/commands.ts
+++ src/commands.ts
@@ -2,13 +2,158 @@ import { Option, autocomplete } from './autocomplete';
 
 const availablePackages: Option[] = [
   {
-    label: 'Unocss',
-    value: 'unocss',
-    group: 'packages',
+    label: 'active-element',
+    value: 'Inputs',
+    group: 'primitives',
   },
   {
-    label: 'unoc222',
-    value: 'unocss22',
+    label: 'audio',
+    value: 'Display & Media',
+    group: 'primitives',
+  },
+  {
+    label: 'autofocus',
+    value: 'Inputs',
+    group: 'primitives',
+  },
+  {
+    label: 'bounds',
+    value: 'Display & Media',
+    group: 'primitives',
+  },
+  {
+    label: 'broadcast-channel',
+    value: 'Browser APIs',
+    group: 'primitives',
+  },
+  {
+    label: 'clipboard',
+    value: 'Browser APIs',
+    group: 'primitives',
+  },
+  {
+    label: 'connectivity',
+    value: 'Network',
+    group: 'primitives',
+  },
+  {
+    label: 'context',
+    value: 'Utilities',
+    group: 'primitives',
+  },
+  {
+    label: 'cursor',
+    value: 'Utilities',
+    group: 'primitives',
+  },
+  // {
+  //   label: 'date',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'destructure',
+  //   value: 'Reactivity',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'devices',
+  //   value: 'Display & Media',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'event-bus',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'event-dispatcher',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'event-listener',
+  //   value: 'Browser APIs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'event-props',
+  //   value: 'Browser APIs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'fetch',
+  //   value: 'Network',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'fullscreen',
+  //   value: 'Browser APIs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'geolocation',
+  //   value: 'Browser APIs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'graphql',
+  //   value: 'Network',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'i18n',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'idle',
+  //   value: 'Display & Media',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'input-mask',
+  //   value: 'Inputs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'intersection-observer',
+  //   value: 'Display & Media',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'jsx-parser',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'keyboard',
+  //   value: 'Inputs',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'keyed',
+  //   value: 'Control Flow',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'map',
+  //   value: 'Utilities',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'media',
+  //   value: 'Display & Media',
+  //   group: 'primitives',
+  // },
+  // {
+  //   label: 'memo',
+  //   value: 'Reactivity',
+  //   group: 'primitives',
+  // },
+  {
+    label: 'Unocss',
+    value: 'unocss',
     group: 'packages',
   },
   {
@@ -17,42 +162,19 @@ const availablePackages: Option[] = [
     group: 'packages',
   },
   {
-    label: 'Primitives',
-    value: 'primitives',
-    group: 'primitives',
+    label: 'Kobalte',
+    value: 'kobalte',
+    group: 'packages',
   },
   {
-    label: 'Primitives',
-    value: 'primitives',
+    label: 'solid-select',
+    value: 'solid-select',
+    group: 'packages',
   },
   {
-    label: 'Primitives',
-    value: 'primitives',
-  },
-  {
-    label: 'Primitives',
-    value: 'primitives',
-  },
-  {
-    label: 'Primitives',
-    value: 'primitives',
-  },
-  {
-    label: 'Primitives',
-    value: 'primitives',
-  },
-  {
-    label: 'sometailFox',
-    value: 'sometail',
-    group: 'primitives',
-  },
-  {
-    label: 'Primitives',
-    value: 'primitives',
-  },
-  {
-    label: '10000 Primitives',
-    value: '100 primitives',
+    label: 'ark',
+    value: 'ark',
+    group: 'packages',
   },
 ];
 
